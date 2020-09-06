7be71ccc10bfa826f892d88277d2ceed835006d4
areyes107 <angelmrg2000@hotmail.com>
2020-09-06 17:53:23 -0600

    app/components/pre-primaria/pre-primaria.component: created new component

---
+++ src/app/components/pre-primaria/pre-primaria.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-primaria',
+  templateUrl: './pre-primaria.component.html',
+  styleUrls: ['./pre-primaria.component.css']
+})
+export class PrePrimariaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
