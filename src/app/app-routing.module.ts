d83b597e2ba01eb1f5edf058136017d7bd5b1fdc
areyes107 <angelmrg2000@hotmail.com>
2020-09-06 17:53:32 -0600

    app/app-routing.module: changed routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CuartoComponent } from './components/cuarto/cuarto.component';
 import { HomeComponent } from './components/home/home.component';
-import { KinderComponent } from './components/kinder/kinder.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-import { PreKinderComponent } from './components/pre-kinder/pre-kinder.component';
-import { PrepaComponent } from './components/prepa/prepa.component';
+import { PrePrimariaComponent } from './components/pre-primaria/pre-primaria.component';
 import { PrimeroComponent } from './components/primero/primero.component';
 import { SegundoComponent } from './components/segundo/segundo.component';
 import { TerceroComponent } from './components/tercero/tercero.component';
@@ -13,9 +11,7 @@ import { TerceroComponent } from './components/tercero/tercero.component';
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'home', component: HomeComponent},
-  { path: 'pre-kinder', component: PreKinderComponent},
-  { path: 'kinder', component: KinderComponent},
-  { path: 'prepa', component: PrepaComponent},
+  { path: 'pre-primaria', component: PrePrimariaComponent},
   { path: 'primero', component: PrimeroComponent},
   { path: 'segundo', component: SegundoComponent},
   { path: 'tercero', component: TerceroComponent},
